458a09ee56d01e1af9a24d3bdb88169df0ab9f3e
ullllosta <lizayakushova172@gmail.com>
2022-04-06 18:11:13 +0300

    commit

--- BD/123.py
+++ BD/123.py
@@ -9,3 +9,11 @@ with sq.connect('saper.db') as con:
         old INTEGER,
         score INTEGER
     )''')
+    info_users = [
+        (1, "Алексей", 1, 22, 1000)
+        (2, "Миша", 1, 19, 800)
+        (3, "Сергей", 1, 19, 900)
+        (4, "Мария", 2, 18, 1500)
+        (5, "Александр", 1, 20, 1100)
+    ]
+    cur.executemany("INSERT INTO users VALUES (?, ?, ?, ?)")
