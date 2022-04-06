8ed795f55fb26966b2572ff649041fb9ee0b3af5
mariia2206 <vlada.ryndia@gmail.com>
2022-04-06 17:50:50 +0300

    commit

---
+++ BD/123.py
@@ -0,0 +1,11 @@
+import sqlite3 as sq
+
+with sq.connect('saper.db') as con:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        sex INTEGER NOT NULL DEFAULT 1,
+        old INTEGER,
+        score INTEGER
+    )''')
